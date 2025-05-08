2632d092bf69baf580dde12e838a6a066938e991
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-08 15:49:39 +0700

    Create application/models/Kerry_setting_model.php

---
+++ application/models/Kerry_setting_model.php
@@ -0,0 +1,6 @@
+<?php
+class Kerry_setting_model extends CI_Model {
+    public $table = 'kerry_settings';
+    public $primary_key = 'setting_id';
+    // ... methods get_settings(), insert(), update() ...
+}
