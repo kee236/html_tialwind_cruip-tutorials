539f1a329359cdace8969c6f491aab1f9e135cf9
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-08 15:56:01 +0700

    Update Shipping_method_model.php

--- application/models/Shipping_method_model.php
+++ application/models/Shipping_method_model.php
@@ -1,14 +1,41 @@
-  <?php
+<?php
 class Shipping_method_model extends CI_Model {
     public $table = 'shipping_methods';
     public $primary_key = 'method_id';
-    // ... methods get_all(), get(), insert(), update(), get_id_by_name(), get_all_ordered() ...
-}
 
- * application/models/Ems_setting_model.php:
-   <?php
-class Ems_setting_model extends CI_Model {
-    public $table = 'ems_settings';
-    public $primary_key = 'setting_id';
-    // ... methods get_settings(), insert(), update() ...
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function get_all() {
+        return $this->db->get($this->table)->result_array();
+    }
+
+    public function get($id) {
+        return $this->db->where($this->primary_key, $id)->get($this->table)->row_array();
+    }
+
+    public function get_where($where) {
+        return $this->db->where($where)->get($this->table)->result_array();
+    }
+
+    public function get_all_ordered() {
+        $this->db->order_by('order', 'ASC');
+        return $this->db->get($this->table)->result_array();
+    }
+
+    public function get_id_by_name($name) {
+        $result = $this->db->where('method_name', $name)->get($this->table)->row_array();
+        return $result ? $result['method_id'] : null;
+    }
+
+    public function insert($data) {
+        return $this->db->insert($this->table, $data);
+    }
+
+    public function update($id, $data) {
+        $this->db->where($this->primary_key, $id);
+        return $this->db->update($this->table, $data);
+    }
 }
