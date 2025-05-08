75b9f31dc69d6d2f2e2deedc700deb989ee66166
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-08 16:18:05 +0700

    Create Payment_gateway_provider_model.php

---
+++ application/models/Payment_gateway_provider_model.php
@@ -0,0 +1,35 @@
+<?php
+class Payment_gateway_provider_model extends CI_Model {
+    protected $table = 'payment_gateway_providers';
+    protected $primaryKey = 'provider_id';
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function get_all() {
+        return $this->db->get($this->table)->result_array();
+    }
+
+    public function get($id) {
+        return $this->db->where($this->primaryKey, $id)->get($this->table)->row_array();
+    }
+
+    public function get_by_name($name) {
+        return $this->db->where('provider_name', $name)->get($this->table)->row_array();
+    }
+
+    public function insert($data) {
+        return $this->db->insert($this->table, $data);
+    }
+
+    public function update($id, $data) {
+        $this->db->where($this->primaryKey, $id);
+        return $this->db->update($this->table, $data);
+    }
+
+    public function delete($id) {
+        return $this->db->where($this->primaryKey, $id)->delete($this->table);
+    }
+}
