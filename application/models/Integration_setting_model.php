5199860f51259072c4a23a99554195b6bbf3f50d
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-08 15:50:01 +0700

    Create application/models/Integration_setting_model.php

---
+++ application/models/Integration_setting_model.php
@@ -0,0 +1,6 @@
+<?php
+class Integration_setting_model extends CI_Model {
+    public $table = 'integration_settings';
+    public $primary_key = 'integration_id';
+    // ... methods get_all(), get(), insert(), update() ...
+}
