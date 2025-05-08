0fec20c9a211b474f24d3df41aeb71c470d400f1
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-08 15:49:15 +0700

    Create application/models/Ems_setting_model.php

---
+++ application/models/Ems_setting_model.php
@@ -0,0 +1,6 @@
+<?php
+class Ems_setting_model extends CI_Model {
+    public $table = 'ems_settings';
+    public $primary_key = 'setting_id';
+    // ... methods get_settings(), insert(), update() ...
+}
